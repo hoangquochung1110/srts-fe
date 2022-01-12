366ef51f284f847218e58673b22475119f0e2864
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-01-12 17:53:41 +0700

    Add custom hooks provide authentication status

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,51 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import { accountService } from '../service/accountService';
+
+
+const authContext = createContext();
+
+/*
+Hook for children components to get the auth object.
+The auth object helps conditional rendering
+*/
+export const useAuth = () => {
+    return useContext(authContext);
+}
+
+/*
+Hook to create the auth object and manage state
+and wrap Facebook SDK
+*/ 
+export const useProvideAuth = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+        window.FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                setUser('user');
+            }
+        })
+    },[]);
+
+    const logIn = () => {
+        setUser('user');
+        accountService.login();
+    }
+
+    const logOut = () => {
+        setUser(null);
+        accountService.logout();
+    }
+
+    return {
+        user,
+        logIn,
+        logOut,
+    }
+}
+
+// A component that makes the auth object available to its children components
+export const ProvideAuth = ({children}) => {
+    const auth = useProvideAuth();
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
