eaf2f948c568d31868ff63f408349c5e54170d5c
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-01-12 17:59:19 +0700

    Put components all together

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
+import { FBSdkInit } from './service/accountService';
+import { ProvideAuth } from './hooks/useAuth';
+import { PrivateRoute } from './components/auth/PrivateRoute';
+import Memories from './memories/Memories';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/login" element={<Login/>}/>
+FBSdkInit().then(startApp);
 
-    </Routes>
-  </BrowserRouter>,
+function startApp() { 
+  render(
+  <ProvideAuth>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route 
+            path="/memories" 
+            element={
+              <PrivateRoute>
+                <Memories/>
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ProvideAuth>,
   document.getElementById('root')
-);
+  );
+}
